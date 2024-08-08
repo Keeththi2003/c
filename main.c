f8428296b5b7358256a886fdc3f4d1ccd6cc4239
Keeththi2003 <keeththi2003@gmail.com>
2024-08-08 14:25:46 +0530

    jjj

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int fun(int n)
+{
+    if (n<=1) {
+        return n;
+    }
+    else{
+        return n*fun(n-2);
+    }
+}
+int main() 
+{
+    printf("anser is %d",fun(4));
+}
+
